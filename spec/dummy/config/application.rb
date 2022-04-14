0733d52b2881d8e74401218fc24f9905ed50b22e
Mufid <mufid@outlook.com>
2022-04-14 21:21:38 +0700

    Rails 7.0 compatibility

--- spec/dummy/config/application.rb
+++ spec/dummy/config/application.rb
@@ -8,5 +8,16 @@ Bundler.require(*Rails.groups)
 
 module Dummy
   class Application < Rails::Application
+    if Rails.version.match('7.0')
+      config.load_defaults 7.0
+    elsif Rails.version.match('6.1')
+      config.load_defaults 6.1
+    elsif Rails.version.match('6.0')
+      config.load_defaults 6.0
+    elsif Rails.version.match('5.2')
+      config.load_defaults 5.2
+    elsif Rails.version.match('5.1')
+      config.load_defaults 5.1
+    end
   end
 end
